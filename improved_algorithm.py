af0c62faabd09461a3d58403a20a5fc2355d38c0
Ghiaser <118222204+Ghiaser@users.noreply.github.com>
2025-07-25 23:10:01 +0300

    improved_algorithm.py

--- improved_algorithm.py
+++ improved_algorithm.py
@@ -1,34 +1,28 @@
-import logging
-from collections import defaultdict
+from algorithm import contiguous_oriented_labeling
 
 class FastGraphicalCakeDivider:
-    def __init__(self, graph, vertex_valuations):
+    def __init__(self, graph, edge_values):
+        """
+        graph: networkx.Graph object
+        edge_values: list of two dicts [{edge: value}, {edge: value}] representing valuations of two agents
+        """
         self.graph = graph
-        self.vertex_valuations = vertex_valuations
-        self.edge_values = self._precompute_edge_values()
-
-    def _precompute_edge_values(self):
-        edge_values = [defaultdict(float) for _ in range(2)]
-        for u, v in self.graph.edges():
-            for agent_id in range(2):
-                val_u = self.vertex_valuations[agent_id].get(u, 0.0)
-                val_v = self.vertex_valuations[agent_id].get(v, 0.0)
-                edge_values[agent_id][(min(u, v), max(u, v))] = val_u + val_v
-        return edge_values
+        self.edge_values = edge_values
 
     def divide(self):
-        # Get contiguous oriented labeling from the original implementation
-        from algorithm import contiguous_oriented_labeling
+        # מקבל את התווית המכילה: (label, i_minus, i_plus)
         directed_edges = contiguous_oriented_labeling(self.graph)
 
         A, B = [], []
         value_A, value_B = 0.0, 0.0
 
-        for (u, v) in directed_edges:
-            edge = (min(u, v), max(u, v))
-            val0 = self.edge_values[0][edge]
-            val1 = self.edge_values[1][edge]
+        # לולאה על שלושה פריטים בכל איבר ברשימה
+        for _, u, v in directed_edges:
+            edge = (min(u, v), max(u, v))  # קשת בלתי מכוונת לפי הסכמה
+            val0 = self.edge_values[0].get(edge, 0)
+            val1 = self.edge_values[1].get(edge, 0)
 
+            # מחלקים את הקשת בין שתי הקבוצות לפי הערך המצטבר
             if value_A <= value_B:
                 A.append(edge)
                 value_A += val0
